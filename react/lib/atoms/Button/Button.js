28ae624222de702499cb82650a0f9067ccae691b
Dee77777 <deepak.aneja77@gmail.com>
2026-09-16 13:12:42 +0530

    more changes

---
+++ react/lib/atoms/Button/Button.js
@@ -0,0 +1,8 @@
+import { jsx } from 'react/jsx-runtime';
+
+const Button = ({ label }) => {
+    return jsx("button", { className: 'dse-button__container', children: label });
+};
+
+export { Button as default };
+//# sourceMappingURL=Button.js.map
---
+++ react/rollup.config.js
@@ -0,0 +1,27 @@
+import Ts from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+export default {
+    input: [
+        'src/index.ts'
+    ],
+    output: {
+        dir: 'lib',
+        preserveModules: true,
+        format: 'esm',
+        sourcemap: true
+    },
+    plugins: [nodeResolve({
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }),
+    Ts({
+            tsconfig: './tsconfig.json',
+            include: ['src/**/*.ts', 'src/**/*.tsx'],
+            clean: true
+    })],
+    external: [
+    'react', 
+    'react-dom', 
+    'react/jsx-runtime' // Add this line
+  ]
+}
